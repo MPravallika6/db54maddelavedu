036e405be87a5b60fdf4171f56583a5a0fde4afd
MPravallika6 <s545254@nwmissouri.edu>
2021-11-10 21:13:09 -0600

    updated

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const connectionString = process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,{useNewUrlParser: true, useUnifiedTopology: true}); 
+
+//Get the default connection 
+var db = mongoose.connection; 
+ 
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+ console.log("Connection to DB succeeded")
+ recreateDB();
+}); 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var textbookRouter = require('./routes/textbook');
+//var mobilesRouter = require('./routes/mobiles');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+const Costume = require("./models/costume");
+const resoureRouter = require('./routes/resource');
+var costumeRouter = require('./routes/costume');
 
 var app = express();
 
@@ -24,9 +41,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/textbook', textbookRouter);
-app.use('/addmods', addmodsRouter);
+//app.use('/mobiles', mobilesRouter);
 app.use('/selector', selectorRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/resource', resoureRouter);
+app.use('/costumes', costumeRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,4 +64,27 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+async function recreateDB(){ 
+  // Delete everything 
+  await Costume.deleteMany(); 
+ 
+  let instance1 = new Costume({costume_type:"Ghost",  size:'large', cost:25.4}); 
+  let instance2 = new Costume({costume_type:"Happy days",  size:'Medium', cost:36.4}); 
+  let instance3 = new Costume({costume_type:"Life is beautiful",  size:'small', cost:42.6}); 
+
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  });
+  instance2.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("second object saved") 
+  });
+  instance3.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("third object saved") 
+  });
+  // let reseed = true; 
+  // if (reseed) { recreateDB();}  
+} 
 module.exports = app;
