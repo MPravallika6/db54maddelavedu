73c631279192ab8c0fef112dfe1d78d56a5baa89
MPravallika6 <s545254@nwmissouri.edu>
2021-11-17 18:39:05 -0600

    updated the files

--- controllers/costume.js
+++ controllers/costume.js
@@ -43,7 +43,7 @@ exports.costume_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id); 
 }; 
  
-// Handle Costume update form on PUT. 
+//Handle Costume update form on PUT. 
 exports.costume_update_put = async function(req, res) { 
     console.log(`update on id ${req.params.id} with body 
 ${JSON.stringify(req.body)}`) 
@@ -62,8 +62,7 @@ ${JSON.stringify(req.body)}`)
         res.send(`{"error": ${err}: Update for id ${req.params.id} 
 failed`); 
     } 
-};
-
+}; 
 
 exports.costume_view_all_Page = async function(req, res) { 
     try{ 
@@ -84,5 +83,74 @@ exports.costume_detail = async function(req, res) {
     } catch (error) { 
         res.status(500) 
         res.send(`{"error": document for id ${req.params.id} not found`); 
-    }
+    } 
 };
+
+// Handle Costume delete on DELETE.
+exports.costume_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Costume.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+
+// Handle a show one view with id specified by query
+exports.costume_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Costume.findById( req.query.id)
+    res.render('costumedetail',
+    { title: 'Costume Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.costume_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('costumecreate', { title: 'Costume Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.costume_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Costume.findById(req.query.id)
+    res.render('costumeupdate', { title: 'Costume Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+   
+// Handle a delete one view with id from query
+exports.costume_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Costume.findById(req.query.id)
+    res.render('costumedelete', { title: 'Costume Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/costume.js
+++ routes/costume.js
@@ -5,3 +5,15 @@ var router = express.Router();
 /* GET costumes */ 
 router.get('/', costume_controlers.costume_view_all_Page ); 
 module.exports = router; 
+
+/* GET detail costume page */
+router.get('/detail', costume_controlers.costume_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', costume_controlers.costume_create_Page);
+
+/* GET create update page */
+router.get('/update', costume_controlers.costume_update_Page);
+
+/* GET create costume page */
+router.get('/delete', costume_controlers.costume_delete_Page);
